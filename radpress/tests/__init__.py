007ef74a00a0118abd0a8a50cebcc4f07f6af3cf
Gokmen Gorgen <gokmen@alageek.com>
2013-03-10 22:53:08 +0200

    detail page tests.

--- radpress/tests/__init__.py
+++ radpress/tests/__init__.py
@@ -1,8 +1,9 @@
 import os.path
+from django.core.urlresolvers import reverse
 from django.template.defaultfilters import slugify
 from django.test import TestCase
 from django.test.client import Client
-from radpress.models import Article, Tag
+from radpress.models import Article, Page, Tag
 from radpress.templatetags.radpress_tags import restructuredtext
 
 
@@ -15,7 +16,22 @@ class Test(TestCase):
         self.article1 = Article.objects.get(pk=1)
 
     def test_all_published_articles(self):
+        # check published article count
         self.assertEqual(Article.objects.all_published().count(), 1)
+
+        # check published page count
+        self.assertEqual(Page.objects.all_published().count(), 2)
+
+    def test_open_private_and_public_article_details(self):
+        for article in Article.objects.all():
+            status_code = 200 if article.is_published else 404
+            response = self.client.get(
+                reverse('radpress-detail', args=[article.slug]))
+
+            self.assertEqual(response.status_code, status_code)
+
+    def test_preview_page(self):
+
 
     def test_slugs(self):
         for article in Article.objects.all():
