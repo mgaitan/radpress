e9fdbcb8dfe309b21139eccaca0dc8220ccf9de1
Gokmen Gorgen <gokmen@alageek.com>
2013-05-19 14:58:45 +0300

    fix message statement.

--- radpress/tests/__init__.py
+++ radpress/tests/__init__.py
@@ -3,5 +3,6 @@ from django.conf import settings
 from radpress.tests.base import BaseTest
 
 if 'django.contrib.admin' in settings.INSTALLED_APPS:
-    print("`django.contrib.admin` is not installed, passed admin tests...")
     from radpress.tests.admin import AdminTest
+else:
+    print("`django.contrib.admin` is not installed, passed admin tests...")
