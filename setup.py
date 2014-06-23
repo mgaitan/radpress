b57279e7572c667ddfe7d4c0873dc00bb97d9c11
Gokmen Gorgen <gokmen@alageek.com>
2014-06-24 00:02:29 +0300

    add missing classifiers.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PROJECT_DIR = os.path.dirname(__file__)
 
 # get requires from requirements/global.txt file.
 requires_file_name = os.path.join(PROJECT_DIR, 'requirements', 'global.txt')
-with file(requires_file_name) as install_requires:
+with open(requires_file_name) as install_requires:
     install_requires = map(lambda x: x.strip(), install_requires.readlines())
 
 try:
@@ -32,9 +32,14 @@ setup(
     zip_safe=False,
     install_requires=install_requires,
     classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Framework :: Django",
-        "Environment :: Web Environment"
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ]
 )
